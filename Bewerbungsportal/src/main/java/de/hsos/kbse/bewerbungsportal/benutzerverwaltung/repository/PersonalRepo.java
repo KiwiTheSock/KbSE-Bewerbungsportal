6c865dac242a2cd19dcdaa97aa845a4836d81499
KiwiTheSock <p.markmann@live.de>
2020-04-29 15:56:54 +0200

    update folder
    pm

---
+++ Bewerbungsportal/src/main/java/de/hsos/kbse/bewerbungsportal/benutzerverwaltung/repository/PersonalRepo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.hsos.kbse.bewerbungsportal.benutzerverwaltung.repository;
+
+/**
+ *
+ * @author PMark
+ */
+public class PersonalRepo {
+    
+    //asdaf
+    
+}
